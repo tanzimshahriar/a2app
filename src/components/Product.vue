07e0f4f281abedefaaf6071451b8c11f774db628
VANLALSIAMA BAWIHRIN <siambawihrin@VANLALSIAMAs-MacBook-Pro.local>
2019-10-16 15:58:05 +1100

    fix product button alignment

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
+    <div class="pd-container">
     <img alt="Product Image not available"  src="../assets/images/asus.jpg"/>
     <h5 class="card-title">{{ name }}</h5>
     <span class="price-new">${{ price }}</span>
     <p>{{ description }}</p>
-    <button type="button" class="btn btn-secondary btn-sm btn-block">
+     </div>
+
+    <button id="pd-Btn" type="button" class="btn btn-secondary btn-sm btn-block">
+     
       Add to Cart
       <img src="../assets/images/cart1.png" width="35px" height="35px" />
     </button>
@@ -74,5 +78,11 @@ export default {
 }
 .product-price {
   font-weight: lighter;
+}
+.pd-container{
+  min-height: 380px;
+}
+#pd-Btn{
+background-color: rgb(57, 116, 77);
 }
 </style>
