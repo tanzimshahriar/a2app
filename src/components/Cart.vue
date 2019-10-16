d8a236ee6e20e47118833ad65aff7808b670a301
tanzimshahriar <tanzimshahriarfahim@gmail.com>
2019-10-16 11:30:29 +1100

    cart modified

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="cart">
     <h1>Your Cart</h1>
+    <div class = "cart-items-container">
+      <div class = "cart-item">
+        Item
+        <div>Quantity
+          <Button class= "quantity-btn">+</Button>
+          <Button class= "quantity-btn">-</Button>
+        </div>
+        <button>Remove</button>
+      </div>
+    </div>
     <div>
       <p>Your Cart is empty</p>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .cart-items-container {
+
+  }
+</style>
